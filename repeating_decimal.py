bc998d6d9ab5197a302a4cd645a3c16d8ce3bb65
Kelke <kelke@uni-bremen.de>
2022-01-22 23:32:37 +0100

    splitting string works

--- repeating_decimal.py
+++ repeating_decimal.py
@@ -4,16 +4,22 @@ import decimal as de
 def split(x, n): # returns a list
     r = divmod(x,1)[1]
     s = str(r)
-    for i in range(1,5):
-        print("round: "+str(i))
+    l = [2, 2+n, 2+n*2]
+    print("List l: "+str(l))
+    ol = [0,0]
 
-    fir = 1+n
-    sec = fir+n
+    for i in range(2):
+        #print()
+        #print(l[i])
+        #print(l[i+1])
+        #print(ol[i])
+        ol[i] = s[l[i]:l[i+1]]
+    print(ol)
     print(s[2:4])
     #l = []
     return
 
 de.getcontext().prec = 100
 rep = de.Decimal(1) / de.Decimal(7)
-split(rep, 0)
+split(rep, 4)
 print(rep)
