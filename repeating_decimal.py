6eae63cc98991882ba8666f691117974325ab234
Kelke <kelke@uni-bremen.de>
2022-01-22 23:48:42 +0100

    refactoring

--- repeating_decimal.py
+++ repeating_decimal.py
@@ -1,25 +1,19 @@
-import numpy as np
 import decimal as de
 
 def split(x, n): # returns a list
     r = divmod(x,1)[1]
     s = str(r)
-    l = [2, 2+n, 2+n*2]
-    print("List l: "+str(l))
-    ol = [0,0]
+    groups = 5
+    pointers = [2]
+    for i in range(1,groups+1):
+        pointers.append(2+n*i)
+    print("Pointers: "+str(pointers))
 
-    for i in range(2):
-        #print()
-        #print(l[i])
-        #print(l[i+1])
-        #print(ol[i])
-        ol[i] = s[l[i]:l[i+1]]
-    print(ol)
-    print(s[2:4])
-    #l = []
-    return
+    ol = []
+    for i in range(groups):
+        ol.append(s[pointers[i]:pointers[i+1]])
+    return ol
 
 de.getcontext().prec = 100
 rep = de.Decimal(1) / de.Decimal(7)
-split(rep, 4)
-print(rep)
+print(split(rep,4))
