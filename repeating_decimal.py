554a2a3204a2d59ab283d0dd5e4b6bbcedbc9ec5
Kelke <kelke@uni-bremen.de>
2022-01-23 01:03:47 +0100

    repdec input working

--- repeating_decimal.py
+++ repeating_decimal.py
@@ -1,38 +1,47 @@
 import decimal as de
 
-def split(x, n): # returns a list
-    r = divmod(x,1)[1]
+def split(x, n):    # returns a list of possibe repeat numbers. x is input number, n is repeat length
+    i, r = divmod(x,1)
     s = str(r)
-    groups = 2
+    if(r == 0):
+        return False
+    groups = 2      # how many times to check repeat number within input number
     pointers = [2]
     for i in range(1,groups+1):
         pointers.append(2+n*i)
-    #print("Pointers: "+str(pointers))
+    #print("Pointers: +str(pointers))
 
     ol = []
     for i in range(groups):
         ol.append(s[pointers[i]:pointers[i+1]])
-    #print(ol)
+    #print("Possible repeat outputs"+ol)
     return ol
 
 de.getcontext().prec = 100
-rep = de.Decimal(1) / de.Decimal(7)
+dividend = int(input("Please enter Dividend: "))
+divisor = int(input("Please enter Divisor: "))
 
-found = False
-for groupsize in range (1,11):
-    groups = split(rep,groupsize)
-    equalgroups = 1
-    #print(len(groups))
-    for i in range(len(groups)-1):
-        if groups[i] == groups[i+1]:
-            equalgroups += 1
-    if equalgroups == len(groups):
-        found = True
-        print("Found repeat at following amount of digits: "+str(groupsize))
-        print("input number: "+str(rep))
-        s = ""
-        for i in range(len(groups)):
-            s += " | "+groups[i]
-        print("repeat number: "+str(rep)[:2]+s)
-        break
-# print("End")
+if(isinstance(dividend, int) and isinstance(divisor, int)):
+    rep = de.Decimal(dividend) / de.Decimal(divisor)
+    print("Input number: "+str(rep))
+
+    found = False
+    for groupsize in range (1,11):
+        groups = split(rep,groupsize)
+        if not groups:
+            break
+        equalgroups = 1
+        #print(len(groups))
+        for i in range(len(groups)-1):
+            if groups[i] == groups[i+1]:
+                equalgroups += 1
+        if equalgroups == len(groups):
+            found = True
+            print("Found repeat at following amount of digits: "+str(groupsize))
+            s = ""
+            for i in range(len(groups)):
+                s += " | "+groups[i]
+            print("Repeat number: "+str(rep)[:2]+s)
+            break
+if not found:
+    print("No repeat found")
