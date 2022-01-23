d0ba409a50df8661cf81bb5023e7f65958c46bfc
Kelke <kelke@uni-bremen.de>
2022-01-23 01:27:08 +0100

    repdec testing movement of starting pointer

--- repeating_decimal.py
+++ repeating_decimal.py
@@ -1,19 +1,24 @@
 import decimal as de
 
-def split(x, n):    # returns a list of possibe repeat numbers. x is input number, n is repeat length
+def split(x, n, s):     # returns a list of possibe repeat numbers.
+    #x is input number  example: 0.181818
+    #n is repeat length example: 2 = 0. 18 18 18
+    #s is start-digit   example: 2 = 0.1 81 81 81
+    groups = 2      # how many times to check repeat number within input number
+    s += 1          # first digit after fraction is r[2]
+
     i, r = divmod(x,1)
-    s = str(r)
+    sr = str(r)         # string r
     if(r == 0):
         return False
-    groups = 2      # how many times to check repeat number within input number
-    pointers = [2]
+    pointers = [s]
     for i in range(1,groups+1):
-        pointers.append(2+n*i)
+        pointers.append(s+n*i)
     #print("Pointers: +str(pointers))
 
     ol = []
     for i in range(groups):
-        ol.append(s[pointers[i]:pointers[i+1]])
+        ol.append(sr[pointers[i]:pointers[i+1]])
     #print("Possible repeat outputs"+ol)
     return ol
 
@@ -21,11 +26,16 @@ de.getcontext().prec = 100
 dividend = int(input("Please enter Dividend: "))
 divisor = int(input("Please enter Divisor: "))
 
-if(isinstance(dividend, int) and isinstance(divisor, int)):
+rep = de.Decimal(dividend) / de.Decimal(divisor)
+pr = split(rep, 2, 2)
+print(pr)
+
+if isinstance(dividend, int) and isinstance(divisor, int) and False:
     rep = de.Decimal(dividend) / de.Decimal(divisor)
     print("Input number: "+str(rep))
 
     found = False
+    #for startdigit in range
     for groupsize in range (1,11):
         groups = split(rep,groupsize)
         if not groups:
@@ -43,5 +53,5 @@ if(isinstance(dividend, int) and isinstance(divisor, int)):
                 s += " | "+groups[i]
             print("Repeat number: "+str(rep)[:2]+s)
             break
-if not found:
-    print("No repeat found")
+#if not found:
+print("No repeat found")
