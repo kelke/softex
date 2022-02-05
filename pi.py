eb25cb5cc38568304442f57b5751cbbe9b71e1df
Kelke <kelke@uni-bremen.de>
2022-02-05 17:12:24 +0100

    trying Nilakantha series

--- pi.py
+++ pi.py
@@ -1,26 +1,45 @@
 #calculate pi using the Gregory-Leibniz Series
 # pi/4 = 1-(1/3)+(1/5)-(1/7)+(1/9)
 
-import decimal as dc
+import decimal
+from decimal import Decimal as dc
 
 def greglei(x): #amount of terms
     divisor = 3
-    pi = dc.Decimal(1)
+    pi = dc(1)
     loopflag = True
     while loopflag:
         for i in range(x):
-            pi -= dc.Decimal(1) / dc.Decimal(divisor)
+            pi -= dc(1) / dc(divisor)
             divisor += 2
-            pi += dc.Decimal(1) / dc.Decimal(divisor)
+            pi += dc(1) / dc(divisor)
             divisor += 2
         if input("Repeat? Y/n ") == "n":
             loopflag = False
-    pi = pi * dc.Decimal(4)
+    pi = pi * dc(4)
     return pi
 
+def nilakantha(repeats: int) -> dc():
+    pi = 3
 
-dc.getcontext().prec = 100000
-loop = int(input("Input number of calculations: "))
-f = open("test.txt", "w")
-f.write(str(greglei(loop)))
-f.close()
+    for base in range (2, repeats*2, 2):
+        prod = base * (base +1) * (base +2)
+        pi += dc(4) / dc(prod)
+        print("base:")
+        print(base)
+        print("prod:")
+        print(prod)
+        print("")
+    return pi
+
+def write_to_file(content: str) -> None:
+    f = open("test.txt", "w")
+    f.write(content)
+    f.close()
+
+if __name__ == '__main__':
+    decimal.getcontext().prec = 1000
+    loop = int(input("Input number of calculations: "))
+    pi = nilakantha(loop)
+    print(pi)
+    write_to_file(str(pi))
