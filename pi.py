051f853bfb0dd715a00dd89f7615c80d666347b7
Kelke <kelke@uni-bremen.de>
2022-02-06 17:11:40 +0100

    montecarlo working
    almost to easy

--- pi.py
+++ pi.py
@@ -2,12 +2,26 @@
 # pi/4 = 1-(1/3)+(1/5)-(1/7)+(1/9)
 
 from decimal import Decimal as d, getcontext as gc
+from sqlite3 import SQLITE_UPDATE
 import time
-import math
-from xmlrpc.client import Boolean
 import pandas as pd
+import random
 
-def nilakantha(repeats: int) -> d():
+def write_to_file(content: str) -> None:
+    f = open("test.txt", "w")
+    f.write(content)
+    f.close()
+
+def compare_string_to_file(string: str, filename: str) -> int:
+    f = open(filename, "r")
+    file_content = f.read()
+    f.close()
+    prec = 1
+    while string[:prec] == file_content[:prec]:
+        prec += 1
+    return prec-1
+
+def nilakantha(loops: int) -> d():
     '''
         Nilakantha Series = 3 + 4/(2 + 3 + 4) - 4/(4 + 5 + 6) + 4/(6 + 7 + 8) - 4/(8 + 9 + 10)
         using modulo
@@ -20,7 +34,7 @@ def nilakantha(repeats: int) -> d():
     base = 2
     switch = True
     
-    for i in range (1, (repeats+1)*2):
+    for i in range (1, (loops+1)*2):
         base = i*2
         prod = base * (base +1) * (base +2)
         if switch:
@@ -52,7 +66,6 @@ def nilakantha_unlim():
                 switch = not switch
                 base += 2
             sim = compare_string_to_file(str(pi),"pi.txt")
-            sim -= 2
             loops += stopafter
             print(f"pi accuracy currently at: {sim} | loops: {loops}")
 
@@ -61,47 +74,66 @@ def nilakantha_unlim():
 
     return pi, loops
 
+def montecarlo(loops: int) -> d():
+    '''
+        imagine circle inside square
+        generate random points on square
+        count points inside and outside of sqare
+    # '''
+    circle_points = 0
+    for square_points in range(loops):
+        x = random.uniform(-1,1)
+        y = random.uniform(-1,1)
+        if x*x + y*y <= 1:
+            circle_points += 1
+    square_points += 1
+    print(f"Of {square_points} points in the square, {circle_points} were inside the circle")
+    pi = d(circle_points)/d(square_points)
+    pi = 4 * pi
+    #pi = d(4) * d(pi)
+    print(pi)
+    return pi
 
+def montecarlo_unlim() -> d():
+    square_points = 0
+    circle_points = 0
+    try:
+        while True:
+            x = random.uniform(-1,1)
+            y = random.uniform(-1,1)
+            if x*x + y*y <= 1:
+                circle_points += 1
+            square_points += 1
+    except KeyboardInterrupt:
+        print("Stopped by Keyboard")
+    print(f"Of {square_points} points in the square, {circle_points} were inside the circle")
+    pi = d(circle_points) / d(square_points)
+    pi = d(4) * d(pi)
+    return pi
 
-def write_to_file(content: str) -> None:
-    f = open("test.txt", "w")
-    f.write(content)
-    f.close()
-
-def compare_string_to_file(string: str, filename: str) -> int:
-    f = open(filename, "r")
-    file_content = f.read()
-    f.close()
-    prec = 1
-    while string[:prec] == file_content[:prec]:
-        prec += 1
-    return prec-1
 
 if __name__ == '__main__':
     #gc().prec = 20000
     #loop = int(input("Input number of calculations: "))
     loop = 10000000  #for runtime testing amount of loops stay same
+    loop = 100000
 
-    precision = 500
+    precision = 50
     gc().prec = precision
 
     start = time.time()
     #pi = nilakantha(loop)
-    pi, loops = nilakantha_unlim()
+    #pi = montecarlo(loop)
+    pi = montecarlo_unlim()
+    #pi, loop = nilakantha_unlim()
     end = time.time()
     runtime = end - start
     runtime = str(runtime)
     pi = str(pi)
     sim = compare_string_to_file(pi, "pi.txt")
     print(f"Similarity: {sim}")
-    print(f"Loops: {loops}")
+    print(f"Loops: {loop}")
     print(f"Precision: {precision}")
     print(f"time: {runtime}")
     print(f"calculated pi: {pi[:sim]}")
     write_to_file(pi)
-    # pi = str(pi)
-
-    #print(str(pi)[:100])
-    #print(f"Runtime: {end - start}")
-    # similarity = compare_string_to_file(str(pi),"pi.txt")
-    # print(similarity)
